a9ac1451abdeaf3987a3d9986a480f8d02ba0f15
husniddin <husniddin@debian.debian>
2025-10-25 20:41:42 +0500

    delete seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,31 +25,31 @@ class DatabaseSeeder extends Seeder
             'chat_id' => 7213131586
         ]);
 
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        \App\Models\Post::factory(20)->create()->each(function ($post) {
-            // Contents
-            \App\Models\Content::factory(rand(3, 4))->create([
-                'post_id' => $post->id,
-            ])->each(function ($content) {
-                // Mediafiles
-                \App\Models\PostMediafile::factory(rand(3, 4))->create([
-                    'content_id' => $content->id,
-                ]);
-            });
+        // \App\Models\Post::factory(20)->create()->each(function ($post) {
+        //     // Contents
+        //     \App\Models\Content::factory(rand(3, 4))->create([
+        //         'post_id' => $post->id,
+        //     ])->each(function ($content) {
+        //         // Mediafiles
+        //         \App\Models\PostMediafile::factory(rand(3, 4))->create([
+        //             'content_id' => $content->id,
+        //         ]);
+        //     });
 
-            // Comments
-            \App\Models\Comment::factory(rand(3, 4))->create([
-                'post_id' => $post->id,
-                'user_id' => \App\Models\User::inRandomOrder()->first()->id,
-            ]);
-        });
+        //     // Comments
+        //     \App\Models\Comment::factory(rand(3, 4))->create([
+        //         'post_id' => $post->id,
+        //         'user_id' => \App\Models\User::inRandomOrder()->first()->id,
+        //     ]);
+        // });
 
-        // Files, Quotes, Contacts
-        \App\Models\File::factory(10)->create();
-        \App\Models\Quote::factory(10)->create();
-        \App\Models\Contact::factory(10)->create([
-            'user_id' => \App\Models\User::inRandomOrder()->first()->id,
-        ]);
+        // // Files, Quotes, Contacts
+        // \App\Models\File::factory(10)->create();
+        // \App\Models\Quote::factory(10)->create();
+        // \App\Models\Contact::factory(10)->create([
+        //     'user_id' => \App\Models\User::inRandomOrder()->first()->id,
+        // ]);
     }
 }
